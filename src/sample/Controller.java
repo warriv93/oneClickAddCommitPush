177658260ef0440f9f9486478cd641fe443cc522
simon <warriv93@gmail.com>
2016-09-20 20:50:56 +0200

    test

---
+++ src/sample/Controller.java
@@ -0,0 +1,57 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Controller {
+
+    /*
+    * Run command in bash and return value
+    * */
+    private String execCommand(String[] commandArr) {
+        String line;
+        String output ="";
+        try
+        {
+            Process p = Runtime.getRuntime().exec(commandArr);
+            BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = stdoutReader.readLine()) != null) {
+                // process procs standard output here
+//                System.out.println(" .. stdout: "+line);
+                output += "\n  "+line;
+            }
+            BufferedReader stderrReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((line = stderrReader.readLine()) != null) {
+                // process procs standard error here
+//                System.err.println(" .. stderr: "+line);
+                output += "\n .. stderr: "+line;
+            }
+            int retValue = p.waitFor();
+//            System.out.println(" .. exit code:"+Integer.toString(retValue));
+            output += "\n .. exit code:"+Integer.toString(retValue);
+        }
+        catch(Exception e)
+        {
+//            System.err.println(e.toString());
+            output += e.toString();
+        }
+
+        return output;
+    }
+
+
+
+    //        Not a git repository
+    public String gitRequest(String directory, String gitCommand){
+        //what to run in the terminal
+        String[] cmd = {
+                "/bin/bash", "-c",
+                "git -C " + directory + " "+ gitCommand +""
+        };
+        return execCommand(cmd);
+    }
+
+
+
+
+}
---
+++ src/sample/Gui.java
@@ -0,0 +1,58 @@
+package sample;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by simon on 9/20/16.
+ */
+public class Gui extends JFrame{
+    private JTextArea textAreaBashOutput;
+    private JButton btGetStatus;
+    private JPanel rootPanel;
+    private JButton btAllInOne;
+    private JTextField TextFieldDirectory;
+
+
+    /*
+    * Initializing gui for application
+    * */
+    public Gui() throws HeadlessException {
+        super("Git Fast Pusher Delux)");
+        Controller controller = new Controller();
+        //set root JPanel
+        setContentPane(rootPanel);
+        //Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
+//        pack();
+//        Center application window location on screen
+        setLocation(800,200);
+        setSize(800,800);
+//        close application on exit
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        btGetStatus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String returnText = controller.gitRequest(TextFieldDirectory.getText(), "status");
+                textAreaBashOutput.setText(returnText);
+            }
+        });
+
+        btAllInOne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.gitRequest(TextFieldDirectory.getText(), "add .");
+                controller.gitRequest(TextFieldDirectory.getText(), "commit -m 'initial commit' ");
+                controller.gitRequest(TextFieldDirectory.getText(), "push origin master");
+            }
+        });
+
+        //Set gui to be visible
+        setVisible(true);
+    }
+
+
+}
